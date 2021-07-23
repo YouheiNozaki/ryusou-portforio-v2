6a23200f6bedc449aa18fb4aa300631477a24253
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-07-23 21:00:37 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { className } from 'styles/home.css';
+
 export default function Home(): JSX.Element {
-  return <div>Hello,Next.js</div>;
+  return <div className={className}>Hello,Next.js</div>;
 }
