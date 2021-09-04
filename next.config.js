faeaf667c4693a8f331464973336df8af9e21ce0
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-09-04 22:50:18 +0900

    vanilla-extractの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,24 +1,69 @@
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+  getGlobalCssLoader,
+} = require('next/dist/build/webpack/config/blocks/css/loaders');
 
-module.exports = {
-  images: {
-    domains: ['images.microcms-assets.io'],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader'],
-    });
-    // ref: https://github.com/seek-oss/vanilla-extract/issues/4#issuecomment-808792494
-    config.plugins.push(
-      new VanillaExtractPlugin(),
-      new MiniCssExtractPlugin({
-        // without these Next.js will look for the generated stylesheets from the wrong place
-        filename: 'static/chunks/[chunkhash].css',
-        chunkFilename: 'static/chunks/[chunkhash].css',
-      }),
-    );
-    return config;
-  },
-};
+/**
+ * @param {ConstructorParameters<typeof VanillaExtractPlugin>[0]} pluginOptions Custom config for vanilla-extract
+ */
+function withVanillaExtract(pluginOptions = {}) {
+  /**
+   * @param {any} nextConfig Custom config for Next.js
+   */
+  return (nextConfig = {}) => {
+    return {
+      images: {
+        domains: ['images.microcms-assets.io'],
+      },
+      webpack5: true,
+      webpack(config, options) {
+        const { dev, isServer } = options;
+
+        config.module.rules.push({
+          test: /\.css$/i,
+          sideEffects: true,
+          use: dev
+            ? getGlobalCssLoader(
+                {
+                  assetPrefix: options.config.assetPrefix,
+                  future: {
+                    webpack5: true,
+                  },
+                  isClient: !isServer,
+                  isServer,
+                  isDevelopment: dev,
+                },
+                [],
+                [],
+              )
+            : [MiniCssExtractPlugin.loader, 'css-loader'],
+        });
+
+        const plugins = [];
+
+        plugins.push(new VanillaExtractPlugin(pluginOptions));
+
+        if (!dev) {
+          plugins.push(
+            new MiniCssExtractPlugin({
+              filename: 'static/css/[contenthash].css',
+              chunkFilename: 'static/css/[contenthash].css',
+              ignoreOrder: true,
+            }),
+          );
+        }
+
+        config.plugins.push(...plugins);
+
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, options);
+        }
+
+        return config;
+      },
+    };
+  };
+}
+
+module.exports = withVanillaExtract()();
