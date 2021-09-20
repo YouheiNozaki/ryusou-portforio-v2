79f1ed1a759f30f765f6137ff6710dda6ba9721d
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-09-21 00:36:04 +0900

    fix

--- src/lib/createOgImage.ts
+++ src/lib/createOgImage.ts
@@ -13,9 +13,7 @@ export const createOgImage = (
   author: Author,
   title: string,
 ) => {
-  const ogImageUrl = `${baseImageUrl}?w=1200&h=1200&mark64=${base64url(
-    `${author.image.url}?w=40&h=40`,
-  )}&mark-x=40&mark-y=80&blend64=${base64url(
+  const ogImageUrl = `${baseImageUrl}?w=1200&h=1200&blend64=${base64url(
     `https://assets.imgix.net/~text?txtsize=48&txt-color=009688&w=1120&txt-align=middle&txtfont=Hiragino%20Sans%20W6&txt-track=2&txt64=${base64url(
       title,
     )}`,
