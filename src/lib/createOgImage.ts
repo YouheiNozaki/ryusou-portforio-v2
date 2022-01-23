4120f8789decf45849f6a52fbf79e8cd8b5d8541
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-01-23 15:45:04 +0900

    blendの位置を修正

--- src/lib/createOgImage.ts
+++ src/lib/createOgImage.ts
@@ -5,7 +5,7 @@ export const createOgImage = (baseImageUrl: string, title: string) => {
     `https://assets.imgix.net/~text?txtsize=48&txt-color=42a5f5&w=1120&txt-align=center&txtfont=Hiragino%20Sans%20W6&txt-track=2&txt64=${base64url(
       title,
     )}`,
-  )}&blend-mode=normal&blend-align=top,left&blend-x=80&blend-y=180`;
+  )}&blend-mode=normal`;
 
   return { ogImageUrl };
 };
