9c15e8ad9cfa70bf4ed1fc733dd9c977fc14c1f6
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-02-06 01:32:18 +0900

    環境変数を修正

--- src/lib/fetchBlogs.ts
+++ src/lib/fetchBlogs.ts
@@ -4,7 +4,7 @@ import type { Blogs, BlogType } from '../types/blogs';
 // Initialize Client SDK.
 export const clientBlogs = createClient({
   serviceDomain: 'ryusou-mtkh',
-  apiKey: process.env.NEXT_PUBLIC__BLOG_API_KEY,
+  apiKey: process.env.BLOG_API_KEY,
 });
 
 export const getBlogList = async (offset?: number) => {
