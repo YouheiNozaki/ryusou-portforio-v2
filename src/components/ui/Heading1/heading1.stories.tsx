087ea8c4b2a31d0a55dcca8fde986e06eb34bd76
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-02-06 22:08:36 +0900

    add heading1 heading2

--- src/components/ui/Heading1/heading1.stories.tsx
+++ src/components/ui/Heading1/heading1.stories.tsx
@@ -1,8 +1,11 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Heading1 } from '.';
 
 export default {
   title: 'components/ui/Heading1',
   component: Heading1,
-};
+} as ComponentMeta<typeof Heading1>;
 
-export const Normal: React.VFC = () => <Heading1 title="Ryusou.dev" />;
+export const Normal: ComponentStory<typeof Heading1> = () => (
+  <Heading1 title="Ryusou.dev" />
+);
--- src/components/ui/Heading2/heading2.stories.tsx
+++ src/components/ui/Heading2/heading2.stories.tsx
@@ -1,8 +1,11 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Heading2 } from '.';
 
 export default {
   title: 'components/ui/Heading2',
   component: Heading2,
-};
+} as ComponentMeta<typeof Heading2>;
 
-export const Normal: React.VFC = () => <Heading2 title="ブログ記事" />;
+export const Normal: ComponentStory<typeof Heading2> = () => (
+  <Heading2 title="ブログ記事" />
+);
