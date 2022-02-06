5612784fe0713081a97f8577bbb8c120cbd27e41
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-02-06 17:48:55 +0900

    add footer storybook

---
+++ src/components/common/Footer/footer.stories.tsx
@@ -0,0 +1,8 @@
+import { Footer } from '.';
+
+export default {
+  title: 'components/common/Footer',
+  component: Footer,
+};
+
+export const Normal: React.VFC = () => <Footer />;
--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,15 +1,15 @@
 import { FiGithub, FiTwitter } from 'react-icons/fi';
-import { footerStyle } from './footer.css';
+import styles from './footer.module.scss';
 
 export const Footer = () => {
   return (
-    <footer className={footerStyle.wrapper}>
-      <div className={footerStyle.icons}>
+    <footer className={styles.wrapper}>
+      <div className={styles.icons}>
         <a href="https://github.com/YouheiNozaki">
-          <FiGithub size={24} className={footerStyle.icon} />
+          <FiGithub size={24} className={styles.icon} />
         </a>
         <a href="https://twitter.com/ryusou_mtkh">
-          <FiTwitter size={24} className={footerStyle.icon} />
+          <FiTwitter size={24} className={styles.icon} />
         </a>
       </div>
     </footer>
