7794db456791b7d0ba4240b29835a3895f7c5ee2
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-02-06 17:39:32 +0900

    heading1 add storybook

---
+++ src/components/ui/Heading1/heading1.stories.tsx
@@ -0,0 +1,8 @@
+import { Heading1 } from '.';
+
+export default {
+  title: 'components/ui/Heading1',
+  component: Heading1,
+};
+
+export const Normal: React.VFC = () => <Heading1 title="Ryusou.dev" />;
